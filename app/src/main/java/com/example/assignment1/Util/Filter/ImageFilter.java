f1f7da2cc98c9cf9af114eedfd6f8647b2924e67
Leon Zhen <leonzhen@gmail.com>
2020-05-05 22:17:38 -0700

    Add filtering functionality
    Signed-off-by: Leon Zhen <leonzhen@gmail.com>

--- app/src/main/java/com/example/assignment1/Util/Filter/ImageFilter.java
+++ app/src/main/java/com/example/assignment1/Util/Filter/ImageFilter.java
@@ -1,10 +1,56 @@
 package com.example.assignment1.Util.Filter;
 
+import android.media.ExifInterface;
+
+import com.example.assignment1.Models.ImageExifModel;
+
 import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class ImageFilter implements Serializable {
     public Date StartDate;
     public Date EndDate;
     public String Caption;
+    private final String DATE_FORMAT = "yyyy-MM-dd HH::mm::ss";
+    /***
+     * Gets a list of filters to filter with
+     * @return a list of filters
+     */
+    public List<Predicate<ImageExifModel>> GetFilters() {
+        List<Predicate<ImageExifModel>> filters = new ArrayList<>();
+
+        // If start date exist add a filter for the start date
+        if (this.StartDate != null) {
+            filters.add(f -> {
+                try {
+                    return new SimpleDateFormat(DATE_FORMAT).parse(f.ExifData.getAttribute(ExifInterface.TAG_DATETIME)).compareTo(this.StartDate) > 0;
+                } catch (ParseException ex) {
+                    return false;
+                }
+            });
+        }
+
+        // If the end date exist add a filter for the end date
+        if (this.EndDate != null) {
+            filters.add(f -> {
+                try {
+                    return new SimpleDateFormat(DATE_FORMAT).parse(f.ExifData.getAttribute(ExifInterface.TAG_DATETIME)).compareTo(this.EndDate) < 0;
+                } catch (ParseException ex) {
+                    return false;
+                }
+            });
+        }
+
+        // If caption filter exist add a filer for caption
+        if(this.Caption != null) {
+            filters.add(f -> f.ExifData.getAttribute(ExifInterface.TAG_IMAGE_DESCRIPTION).contains(this.Caption));
+        }
+
+        return filters;
+    }
 }
