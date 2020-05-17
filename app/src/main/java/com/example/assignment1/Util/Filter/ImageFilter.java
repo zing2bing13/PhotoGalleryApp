f9aa46b31c955a50e8ab51c10ebe3ffd84acce4e
zing2bing13 <zing2bing13@gmail.com>
2020-05-16 21:14:27 -0700

    Date search still not working, but it is parsing the date out of the Exif metadata from the image file.  It is not yet comparing against the value supplied by the user.

--- app/src/main/java/com/example/assignment1/Util/Filter/ImageFilter.java
+++ app/src/main/java/com/example/assignment1/Util/Filter/ImageFilter.java
@@ -1,12 +1,14 @@
 package com.example.assignment1.Util.Filter;
 
 import android.media.ExifInterface;
+import android.util.Log;
 
 import com.example.assignment1.Models.ImageExifModel;
 
 import java.io.Serializable;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -24,11 +26,36 @@ public class ImageFilter implements Serializable {
     public List<Predicate<ImageExifModel>> GetFilters() {
         List<Predicate<ImageExifModel>> filters = new ArrayList<>();
 
+        // Issue with parsing the EXIF data from the image to convert it to a format Date can compare to
         // If start date exist add a filter for the start date
         if (this.StartDate != null) {
             filters.add( f -> {
                 try {
-                    return new SimpleDateFormat(DATE_FORMAT).parse(f.ExifData.getAttribute(ExifInterface.TAG_DATETIME)).compareTo(this.StartDate) > 0;
+
+                    String dateString = f.ExifData.getAttribute(ExifInterface.TAG_DATETIME);
+                    Log.d("error_date", f.ExifData.getAttribute(ExifInterface.TAG_DATETIME));
+
+                    String dateParts[] = dateString.split("[: ]");
+                    Log.d("dateString_length", dateParts[0]);
+                    int year = Integer.parseInt(dateParts[0]);
+                    int month = Integer.parseInt(dateParts[1]);
+                    int day = Integer.parseInt(dateParts[2]);
+                    int hour = Integer.parseInt(dateParts[3]);
+                    int minute = Integer.parseInt(dateParts[4]);
+                    int second = Integer.parseInt(dateParts[5]);
+
+                    SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
+                    Date imageDate = new Date(year, month, day, hour, minute, second);
+                    //Log.d("new_date", imageDate.toString());
+                    //imageDate = sdf.format(imageDate);
+
+                    Log.d("error_date", f.ExifData.getAttribute(ExifInterface.TAG_DATETIME));
+                    SimpleDateFormat date = new SimpleDateFormat(DATE_FORMAT);
+                    date.parse(f.ExifData.getAttribute(ExifInterface.TAG_DATETIME));
+                    Log.d("error_date", date.toString());
+                    Log.d("split_date", imageDate.toString());
+
+                    return new SimpleDateFormat(DATE_FORMAT).format(imageDate).compareTo(this.StartDate.toString()) > 0;
                 } catch (ParseException ex) {
                     return false;
                 }
